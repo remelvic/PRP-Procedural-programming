ba3640f8ebd8c5f9bd4bc0ee2fd87097f4f34cdc
remelvic <fvsap15@gmail.com>
2022-09-16 18:36:32 +0200

    Change hw4, push working code

--- hw4/b0b36prp-hw04/main.c
+++ hw4/b0b36prp-hw04/main.c
@@ -3,56 +3,49 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define MAX_FOR_MANDATORY 100000000
 #define MAX_NUMBERS_SIEVE 1000000
-
 #define ERROR_INPUT 100
+ 
+int sieve_array[MAX_NUMBERS_SIEVE];
+int primes_index = 0;
 
 // The function takes values from the console
 long long read_input();
 
-int *prime_numbers_array;
-int sieve_array[MAX_NUMBERS_SIEVE];
-int size_sieve = 0;
-//Decomposes a number into prime numbers
-void decompose(long long number); //mandatory
+//Decomposes a number into prime numbers. For mandatory and optional parts
+void decompose(long number, int* prime_array, int size);
 
-void decompose_opt(long long number, int* prime_array, int size);
+//I return the number of primes in the array. 
+//Prime_array in all places has either 0 or prime numbers up to 1000000
+int sieve();
 
-//void sieve(long int number);
-void sieve(long long number);
+//Initializing an array to find prime numbers
+void init_prime_array();
 
-int main(int argc, char *argv[])
-{
-  long long number;
+//get all prime numbers 
+int* get_prime_numbers(int* prime_array);
+
+int main(int argc, char *argv[]){
+
+  long number;
+
+  init_prime_array();
   
+  primes_index = sieve();
+  
+  int prime_array[primes_index];
+  
+  int *prime_numbers_array = get_prime_numbers(prime_array);
+
   while((number = read_input()) > 0){
     
-    printf("Prvociselny rozklad cisla %lld je:\n",number);
+    printf("Prvociselny rozklad cisla %ld je:\n",number);
     
     if (number == 1){
       printf("1\n");
     }else{
       
-      // prime_numbers_array = (int*)calloc(MAX_NUMBERS_SIEVE,sizeof(int)); 
-
-      
-      sieve(number);
-      printf("ALOOOOOO %d\n",size_sieve); 
-      // for(int i = 0; i < size_sieve; i++){
-      //   printf("%d ",prime_numbers_array[i]);
-      // }
-      for(int i = 0; i < MAX_NUMBERS_SIEVE; i++){
-        printf("array_end %d \n",sieve_array[i]);
-        if(sieve_array[i] == 1) {
-      //     printf("%d ", i);
-          prime_numbers_array[i] = i;
-          size_sieve++;
-        }
-      }
-      decompose_opt(number,prime_numbers_array, size_sieve);
-      
-      
+      decompose(number,prime_numbers_array, primes_index);
     }
     
   }
@@ -61,87 +54,82 @@ int main(int argc, char *argv[])
     return ERROR_INPUT;
   }
   
-  free(prime_numbers_array);
   return 0;
 }
 
-long long read_input(){
-  long long number;
-  return (scanf("%lld", &number) != 1) ? -1 : number;
+void init_prime_array(){
+  for(int i = 0; i < MAX_NUMBERS_SIEVE; i++){
+    if (i <= 1){
+      sieve_array[i] = 0;
+    }else {
+      sieve_array[i] = i;
+    }
+  }
 }
 
-void decompose_opt(long long number, int* prime_array, int size){
-  _Bool space = false;
-   
-  for(int i = 2; i <= size; ++i){
-    int count = 0;
-    while(number % prime_array[i] == 0){
-      
+long long read_input(){
+  long number;
+  return (scanf("%ld", &number) != 1) ? -1 : number;
+}
 
-      number = number / prime_array[i];
-      
+int sieve(){
+	int count = 0;
+	
+	for (int i = 0; i * i < MAX_NUMBERS_SIEVE; ++i){
+		if (sieve_array[i] != 0){
+			for(int k = (2 * i); k < MAX_NUMBERS_SIEVE; k += i){
+				sieve_array[k] = 0;
+			}
+		}
+	}
+	for(int i = 0; i < MAX_NUMBERS_SIEVE; i++){
+		if (sieve_array[i] != 0){
+			count++;
+		}
+	}
+	return count;
+}
+
+int* get_prime_numbers(int* prime_array){
+  //get numbers from sieve
+  int count = 0;
+  for(int i = 2; i < MAX_NUMBERS_SIEVE; i++){
+    
+    if(sieve_array[i] != 0) {  
+      prime_array[count] = sieve_array[i];
       count++;
     }
+  }
+  return prime_array;
+}
+
+void decompose(long number, int prime_array[], int size){
+  _Bool space = false;
+   
+  for(int i = 0; i <= size; ++i){
+    int count = 0;  
+    while(number % prime_array[i] == 0){
+      
+      number = number / prime_array[i];
+      count++;
+    }
+    
     if(count > 0){
       size = sqrt(size);
       if(space){
         printf(" x ");
       }
-      count > 1 ? printf("%d^%d", prime_array[i], count) : printf("%d",prime_array[i]);
+      if (count > 1) { 
+        printf("%d^%d", prime_array[i], count); 
+      } else {
+        printf("%d",prime_array[i]);
+      }
       space = true; 
     }
   }
   if(number > 1){
     if (space) printf(" x ");
-    printf("%lld",number);
+    printf("%ld",number);
   }
   printf("\n");
 }
-
-// void sieve(long number, long *sieve_array){
-  
-//   for(int i = 1; i < number; i++){
-//     sieve_array[i] = 1;
-//   }
-//   for(int i = 2; i <= sqrt(number); i++){
-//     int count = 0;
-//     printf("SIEVE_ARRAY ELEMENT %ld\n", sieve_array[i]);
-//     if(sieve_array[i] == 1){
-      
-//       for(int j = i*i; j <= number; j = (i*i)+count*i){      
-//         count++;
-//         sieve_array[j] = 0;  
-//       }
-//     }
-//   }
-// }
-
-void sieve(long long number){
-  prime_numbers_array = (int*)calloc(MAX_NUMBERS_SIEVE,sizeof(int)); 
-
-  for(int i = 0; i < MAX_NUMBERS_SIEVE; i++){
-    sieve_array[i] = 1;
-  }
-  
-  for(int k = 2; k < MAX_NUMBERS_SIEVE; k++){
-    int count = 0;
-    //if(sieve_array[k] != 0){
-      // printf("sieve_array %d -- index i: %d\n",sieve_array[k], k);
-      
-      // prime_numbers_array[size_sieve++] = sieve_array[k];
-      // for(int j = k; j < MAX_NUMBERS_SIEVE; j = j * 2){
-      //   sieve_array[j] = 0;
-      // }
-      if(sieve_array[k] == 1){
-        for(int j = k*k; j < number && j < MAX_NUMBERS_SIEVE; j = (k*k)+count*k){      
-          //prime_numbers_array[size_sieve++] = sieve_array[k];
-          count++;
-          if(j < MAX_NUMBERS_SIEVE){
-            sieve_array[j] = 0;
-            
-            //prime_numbers_array[size_sieve++] = j;
-          }
-        }
-      }
-  }
-} 
